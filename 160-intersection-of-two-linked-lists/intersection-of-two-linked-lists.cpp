58e3fde382eb6eedc4602a406b7d61cf84a1a0c5
Anurag Dubey <147299537+anurag9682@users.noreply.github.com>
2025-07-29 23:06:34 +0530

    Time: 41 ms (35.85%) | Memory: 21.3 MB (14.65%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB) return nullptr;
+
+        // ListNode* ptrA= headA;
+        // ListNode* ptrB = headB;
+
+        // while(ptrA!=ptrB){
+
+        //     ptrA = ptrA?ptrA->next:headB;
+        //     ptrB = ptrB?ptrB->next : headA;
+        // }
+        // return ptrA;
+
+        unordered_set<ListNode*>visited;
+        ListNode* cur = headA;
+        while(cur){
+            visited.insert(cur);
+            cur = cur->next;
+        }
+        cur = headB;
+        while(cur){
+            if(visited.find(cur)!= visited.end()){
+                return cur;
+            }
+            cur = cur->next;
+        }
+        return nullptr;
+        
+    }
+};
