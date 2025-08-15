4dda418ac0afef502b39edcb3df0abaa04a0afee
Anurag Dubey <147299537+anurag9682@users.noreply.github.com>
2025-08-16 00:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.41%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n<=0) return false;   // If n is non-positive, return false immediately.
-
-        while(n%4==0){  //Keep dividing n by 4 as long as it is divisible.
+        if(n<=0) return false;
+        while(n%4==0){
             n/=4;
         }
-        return n==1;  //If after division n becomes 1, return true, meaning n was a power of three. otherwise return false
-        
+        return n==1;
     }
 };
