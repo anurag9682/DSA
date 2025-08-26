437bd91573ede487fdcaaac47e68e56d44c448d1
Anurag Dubey <147299537+anurag9682@users.noreply.github.com>
2025-08-26 21:01:51 +0530

    Time: 1 ms (24.08%) | Memory: 29.4 MB (52.12%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -8,7 +8,7 @@ public:
         for(int i =0;i<n;i++ ){
             int num1 = dimensions[i][0];
             int num2 = dimensions[i][1];
-            long long curDiagonal = 1LL*(num1*num1)+1LL*(num2*num2);
+            long long curDiagonal = (num1*num1)+(num2*num2);
 
             if(curDiagonal>diagonalLength){
                 diagonalLength = curDiagonal;
